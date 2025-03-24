af01a1a43aced6ec0cc5e301b019d08e772560f2
seguraabc <122451838+seguraabc@users.noreply.github.com>
2025-03-24 18:19:19 -0300

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Usamos las mismas constantes que están en src/integrations/supabase/client.ts
-const supabaseUrl = 'https://gwikxlahhmjypjlrrdkl.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3aWt4bGFoaG1qeXBqbHJyZGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzMTUyMTgsImV4cCI6MjA1Nzg5MTIxOH0.-b9va7NqOalKIRNH9zgH7QEdJJpOyq4aCYtSEGa8IfY';
+const supabaseUrl = 'https://rjaezvlosvfccrkbtmfo.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqYWV6dmxvc3ZmY2Nya2J0bWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NTAxNTgsImV4cCI6MjA1ODQyNjE1OH0.SY8pCT5l7hV9-eBNB3gUeZpEzz8AfnPSgrtg5LqhNCg';
 
 // Optimizamos la configuración del cliente
 const supabase = createClient(supabaseUrl, supabaseKey, {
